36843677080ef58833539907420991827d244a3e
𖢵 || 𝗦ʏɴαxx ||𖢵 <159287148+Sagexdd@users.noreply.github.com>
2025-02-23 09:54:11 +0530

    Update antiinvite.js

--- events/automod/antiinvite.js
+++ events/automod/antiinvite.js
@@ -41,7 +41,7 @@ module.exports = (client) => {
         .then(() => {
           message.channel.send({embeds: [new client.emb().desc(`${client.emoji.tick} Muted ${message.author.tag} for advertising`).setAuthor({name: message.author.username, iconURL: message.author.displayAvatarURL({dynamic: true})})]})
 
-          ch.send({embeds: [new client.emb().desc(`**${client.emoji.warn} Member Muted**\n**Member**: ${message.author.tag}\n**Member Id**: ${message.author.id}\n\n**Reason**: Advertising`).setAuthor({name: `Automod Action`, iconURL: message.author.displayAvatarURL({dynamic: true})}).setFooter({text: `Kranton Automod`, iconURL: client.user.displayAvatarURL()}).setTimestamp()]}).catch(() => { });
+          ch.send({embeds: [new client.emb().desc(`**${client.emoji.warn} Member Muted**\n**Member**: ${message.author.tag}\n**Member Id**: ${message.author.id}\n\n**Reason**: Advertising`).setAuthor({name: `Automod Action`, iconURL: message.author.displayAvatarURL({dynamic: true})}).setFooter({text: `Destroyer Automod`, iconURL: client.user.displayAvatarURL()}).setTimestamp()]}).catch(() => { });
           
         }).catch((err) => {
         message.channel.send({embeds: [new client.emb().desc(`*Could not timeout ${message.author}*\n${err}`)]});
